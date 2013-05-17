d9160db871c0282cf019f6fb8c352631debef139
Mathieu Baron <mbaron@mirego.com>
2013-05-17 14:06:48 -0400

    Setup test framework

---
+++ MCUIViewLayoutExample/UIViewLayoutExampleTests/MCUIViewLayoutPositionTest.m
@@ -0,0 +1,17 @@
+//
+//  MCUIViewLayoutPositionTest.m
+//  UIViewLayoutExample
+//
+//  Created by Mathieu Baron on 05/17/13.
+//  Copyright (c) 2013 Mirego, Inc. All rights reserved.
+//
+
+#import "MCUIViewLayoutPositionTest.h"
+
+@implementation MCUIViewLayoutPositionTest
+
+- (void) testDummy {
+    STAssertTrue(false, @"");
+}
+
+@end
